b69be31af6b11f68c9e17ddf798f5ca39fe5c0ff
iFallenHunt <lukas_santos.silva@hotmail.com>
2022-07-02 11:16:40 -0300

    converte Euros em Reais

---
+++ conversão.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+float conversao(float qtde, float cot){
+	float conv;
+	conv = qtde * cot;
+	return (conv);
+}
+int main(){
+	float R, E, C;
+	printf("Digite o valor em Euros: ");
+	scanf("%f", &E);
+	printf("\nInsira o valor da cotacao: ");
+	scanf("%f", &C);
+	
+	R = conversao(E,C);
+	
+	printf("O valor em reais e: ", R);
+}
